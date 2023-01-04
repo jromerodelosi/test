2bf34af78aacaf91a5b65e7150ece95a1b1bf92c
Nick Correa García <ncorrea@FRANQUICIASPERU-2.local>
2023-01-04 18:40:07 -0500

    Update Home

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -3,7 +3,6 @@ import classes from "./Home.module.scss";
 export default function Home() {
   return (
     <div className={classes.home}>
-      <h2>Hola soy Home</h2>
       <div className={classes.app}>
         <h1>Nick Test SASS</h1>
         <h2>APP Test</h2>
